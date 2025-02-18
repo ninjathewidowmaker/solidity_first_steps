2509b85a314dc3fbb08faacc1023a2a90dd575f8
ninjathewidowmaker <balummiryala4@yahoo.com>
2025-02-18 22:22:48 +0530

    Add files via upload

---
+++ compact_addition.sol
@@ -0,0 +1,13 @@
+////SPDX-License-Identifier: MIT
+
+//Decentralized Social Media Project - "Social Media App"
+
+pragma solidity ^0.8.26;
+
+contract addition {
+
+function add(int _a, int_b) public pure returns (int sum) {
+    return _a + _b;
+}   
+}
+
---
+++ subtraction_in_solidity.sol
@@ -0,0 +1,12 @@
+////SPDX-License-Identifier: MIT
+
+//Decentralized Social Media Project - "Social Media App"
+
+pragma solidity ^0.8.26;
+
+contract addition {
+
+function add(int _a, int _b) public pure returns (int) {
+    return _a - _b;
+}   
+}
